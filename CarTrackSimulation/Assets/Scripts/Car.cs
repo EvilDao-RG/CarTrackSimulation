ab0a26be8938c212411876388e444d162b20045b
Gael <gaelrodguez@hotmail.com>
2022-11-17 18:45:37 -0600

    Avance clase 17 Nov

---
+++ CarTrackSimulation/Assets/Scripts/Car.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Car
+{
+
+    public int id;
+    public int x;
+    public int y;
+    public int z;
+
+}
